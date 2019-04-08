6e27712bf006a81bd41152475705de01eb8652e8
liwbiy <liwbiy@163.com>
2019-04-08 19:14:38 +0800

    十进制、十六进制数字打印

--- drivers/console.c
+++ drivers/console.c
@@ -38,57 +38,127 @@ static void scroll()
 
     if (cursor_y >= 25)
     {
-        for (uint16_t  i = 0; i < 24 * 80; ++i)
+        for (uint16_t i = 0; i < 24 * 80; ++i)
             video_memory_start[i] = video_memory_start[i + 80];
 
         for (uint16_t i = 24 * 80; i < 25 * 80; ++i)
             video_memory_start[i] = blank;
 
-    cursor_y = 24;
+        cursor_y = 24;
     }
 }
 
 void console_putc_color(char c, real_color_t back, real_color_t fore)
 {
-	uint8_t back_color = (uint8_t)back;
-	uint8_t fore_color = (uint8_t)fore;
+    uint8_t back_color = (uint8_t)back;
+    uint8_t fore_color = (uint8_t)fore;
 
-	uint8_t attribute_byte = (back_color << 4) | (fore_color & 0x0F);
-	uint16_t attribute_word = attribute_byte << 8;
+    uint8_t attribute_byte = (back_color << 4) | (fore_color & 0x0F);
+    uint16_t attribute_word = attribute_byte << 8;
 
-	if (c == '\b' && cursor_x) {
-	    --cursor_x;
-	} else if (c == 0x09) {
-	      cursor_x = (cursor_x+8) & ~(8-1);
-	} else if (c == '\r') {
-	    cursor_x = 0;
-	} else if (c == '\n') {
-	    cursor_x = 0;
-		++cursor_y;
-	} else if (c >= ' ') {
-		video_memory_start[cursor_y*80 + cursor_x] = c | attribute_word;
-		++cursor_x;
-	}
+    if (c == '\b' && cursor_x)
+    {
+        --cursor_x;
+    }
+    else if (c == 0x09)
+    {
+        cursor_x = (cursor_x + 8) & ~(8 - 1);
+    }
+    else if (c == '\r')
+    {
+        cursor_x = 0;
+    }
+    else if (c == '\n')
+    {
+        cursor_x = 0;
+        ++cursor_y;
+    }
+    else if (c >= ' ')
+    {
+        video_memory_start[cursor_y * 80 + cursor_x] = c | attribute_word;
+        ++cursor_x;
+    }
 
-	if (cursor_x >= 80) {
-		cursor_x = 0;
-		++cursor_y;
-	}
+    if (cursor_x >= 80)
+    {
+        cursor_x = 0;
+        ++cursor_y;
+    }
 
-	scroll();
-	move_cursor();
+    scroll();
+    move_cursor();
 }
 
 void console_write(char *cstr)
 {
-    while(*cstr){
+    while (*cstr)
         console_putc_color(*cstr++, rc_black, rc_white);
-    }
 }
 
 void console_write_color(char *cstr, real_color_t back, real_color_t fore)
 {
-    while(*cstr){
+    while (*cstr)
         console_putc_color(*cstr++, back, fore);
+}
+
+void console_write_hex(uint32_t n, real_color_t back, real_color_t fore)
+{
+    char head_are_zeros = 1;
+    console_write_color("0x", back, fore);
+
+    for (uint8_t i = 28; i >= 0; i -= 4)
+    {
+        uint32_t temp = (n >> i) & 0x0F;
+        if ((temp == 0) && head_are_zeros)
+            continue;
+
+        head_are_zeros = 0;
+        if (temp >= 0x0A)
+            console_putc_color((temp - 0x0A + 'a'), back, fore);
+        else
+            console_putc_color(temp + '0', back, fore);
+    }
+}
+
+void console_write_dec(uint32_t n, real_color_t back, real_color_t fore)
+{
+    char head_are_zeros = 1;
+
+    for (uint8_t i = 28; i >= 0; i -= 4)
+    {
+        uint32_t temp = (n >> i) & 0x0F;
+        if ((temp == 0) && head_are_zeros)
+            continue;
+
+        head_are_zeros = 0;
+        if (temp >= 0x0A)
+        {
+            switch (temp)
+            {
+            case 0x0A:
+                console_write("10");
+                break;
+            case 0x0B:
+                console_write("11");
+                break;
+            case 0x0C:
+                console_write("12");
+                break;
+            case 0x0D:
+                console_write("13");
+                break;
+            case 0x0E:
+                console_write("14");
+                break;
+            case 0x0F:
+                console_write("15");
+                break;
+            default:
+                console_write("error!");
+                break;
+            }
+        }
+        else
+            console_putc_color(temp + '0', back, fore);
     }
 }
--- init/entry.c
+++ init/entry.c
@@ -5,6 +5,10 @@ int kern_entry()
     console_clear();
 
     console_write_color("check console function!\n", rc_black, rc_green);
+
+    console_write_dec(32367, rc_black, rc_white);
+
+    console_write_hex(32367, rc_black, rc_white);
     
     return 0;
 }
