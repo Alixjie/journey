ad779f00b803a2bdc8afc8fe9126c1254d3fcbb8
Alixjie <787115192@qq.com>
2019-04-13 20:27:58 +0800

    更新idt.h

--- include/idt.h
+++ include/idt.h
@@ -1,45 +1,47 @@
-#ifndef INCLUDE_IDT_H
-#define INCLUDE_IDT_H
+#ifndef INCLUDE_IDT_H_
+#define INCLUDE_IDT_H_
 
 #include "types.h"
 
+// 初始化中断描述符表
 void init_idt();
 
-typedef struct idt_entry_t
-{
-    uint16_t base_lo; //中断处理函数地址15~0位
-    uint16_t sel;     //目标代码段描述符选择子
-    uint8_t always0;  //置0段
-    uint8_t flags;    // 一些标志
-    uint16_t base_hi; // 中断处理函数地址 31 ~ 16 位
-} __attribute__((packed)) idt_entry_t;
+// 中断描述符
+typedef
+struct idt_entry_t {
+	uint16_t base_lo;        // 中断处理函数地址 15～0 位
+	uint16_t sel;            // 目标代码段描述符选择子
+	uint8_t  always0;        // 置 0 段
+	uint8_t  flags;          // 一些标志，文档有解释
+	uint16_t base_hi;        // 中断处理函数地址 31～16 位
+}__attribute__((packed)) idt_entry_t;
 
-typedef struct idt_ptr_t
-{
-    uint16_t limit; //限长
-    uint32_t base;  //基址
+// IDTR
+typedef
+struct idt_ptr_t {
+	uint16_t limit; 	// 限长
+	uint32_t base; 		// 基址
 } __attribute__((packed)) idt_ptr_t;
 
 // 寄存器类型
-typedef struct pt_regs_t
-{
-    uint32_t ds;  // 用于保存用户的数据段描述符
-    uint32_t edi; // 从 edi 到 eax 由 pusha 指令压入
-    uint32_t esi;
-    uint32_t ebp;
-    uint32_t esp;
-    uint32_t ebx;
-    uint32_t edx;
-    uint32_t ecx;
-    uint32_t eax;
-    uint32_t int_no;   // 中断号
-
-    uint32_t err_code; // 错误代码(有中断错误代码的中断会由CPU压入)
-    uint32_t eip;      // 以下由处理器自动压入
-    uint32_t cs;
-    uint32_t eflags;
-    uint32_t useresp;
-    uint32_t ss;
+typedef
+struct pt_regs_t {
+	uint32_t ds;		// 用于保存用户的数据段描述符
+	uint32_t edi; 		// 从 edi 到 eax 由 pusha 指令压入
+	uint32_t esi; 
+	uint32_t ebp;
+	uint32_t esp;
+	uint32_t ebx;
+	uint32_t edx;
+	uint32_t ecx;
+	uint32_t eax;
+	uint32_t int_no; 	// 中断号
+	uint32_t err_code;  	// 错误代码(有中断错误代码的中断会由CPU压入)
+	uint32_t eip;		// 以下由处理器自动压入
+	uint32_t cs; 		
+	uint32_t eflags;
+	uint32_t useresp;
+	uint32_t ss;
 } pt_regs;
 
 // 定义中断处理函数指针
@@ -91,4 +93,4 @@ void isr31();
 // 32～255 用户自定义异常
 void isr128();
 
-#endif
+#endif 	// INCLUDE_IDT_H_
