570629b49f40cd745678452b52d8d26b32648e46
liwbiy <liwbiy@163.com>
2019-04-17 12:32:48 +0800

    改错

--- mm/pmm.c
+++ mm/pmm.c
@@ -5,16 +5,16 @@
 // 输出 BIOS 提供的物理内存布局
 void show_memory_map()
 {
-    uint32_t mmap_addr = glb_mboot_ptr->addr;
-    uint32_t mmap_length = glb_mboot_ptr->mmap_length;
+	uint32_t mmap_addr = glb_mboot_ptr->mmap_addr;
+	uint32_t mmap_length = glb_mboot_ptr->mmap_length;
 
-    printk("Memory map:\n");
+	printk("Memory map:\n");
 
-    for (mmap_entry_t *mmap_entry = mmap_addr; mmap_entry < mmap_addr + mmap_length; ++mmap_entry)
-    {
-        printk("base_addr = 0x%08X%08X, length = 0x%08X%08X, type = 0x%X\n",
-               (uint32_t)mmap_entry->base_addr_high, (uint32_t)mmap_entry->base_addr_low,
-               (uint32_t)mmap_entry->length_high, (uint32_t)mmap_entry->length_low, 
-               (uint32_t)mmap_entry->type);
-    }
+	mmap_entry_t *mmap = (mmap_entry_t *)mmap_addr;
+	for (mmap = (mmap_entry_t *)mmap_addr; (uint32_t)mmap < mmap_addr + mmap_length; mmap++) {
+		printk("base_addr = 0x%X%08X, length = 0x%X%08X, type = 0x%X\n",
+			(uint32_t)mmap->base_addr_high, (uint32_t)mmap->base_addr_low,
+			(uint32_t)mmap->length_high, (uint32_t)mmap->length_low,
+			(uint32_t)mmap->type);
+	}
 }
